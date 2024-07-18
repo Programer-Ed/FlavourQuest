e0c31aebde6c5bd6f43e863051a48dd4751638ed
Edwin <moyiedwin8@gmail.com>
2024-07-18 15:29:19 +0300

    web recipe

---
+++ README.md
@@ -0,0 +1,29 @@
+## Recipe Search App
+
+This is a recipe search application built with HTML, CSS, JavaScript. It allows users to search for recipes using an API and view the ingredients and instructions for each recipe.
+
+## Features
+
+- User can enter a meal name to search for recipes of the meal.
+- User can view a list of recipes matching the searched meal.
+- User can view the recipe details, including the recipe name, image, area, ingredients, and instructions.
+
+## Installation
+
+1. Clone the repository: `https://github.com/Programer-Ed/recipeProject.git`
+2. Open the project folder: `cd recipeProject1`
+3. Open the `index.html` file in your web browser.
+
+## Usage
+
+1. Enter a meal name in the search input field.
+2. Click the "Search" button to search for recipes.
+3. The search results will be displayed below the header.
+
+## API
+
+This application uses the "The Meal DB" API to fetch recipe data. You can find more information about the API (https://www.themealdb.com/api.php).
+
+# Authors and acknowledgment
+
+This webpage is made,edited and reviewed by Edwin Ng'ang'a 
---
+++ recipe.js
@@ -0,0 +1,71 @@
+const searchInput = document.getElementById("searchInput");
+const searchBtn = document.getElementById("searchButton");
+const result = document.getElementById("results");
+
+searchBtn.addEventListener('click',searchRecipe);
+
+//function to handle recipe search
+function searchRecipe(){
+  //search meal from input
+    const searchMeals = searchInput.value;
+   
+   //when you click search and input is empty
+    if(searchMeals === ''){
+    return result.innerHTML = '<p>Please search for a meal</p>'
+   }
+   
+   //This is an API from mealdb  
+   const url = `https://www.themealdb.com/api/json/v1/1/search.php?s=${searchMeals}`
+
+   //fetch the data from url
+   fetch(url)
+   .then((res)=>res.json())
+   .then((data)=> {
+    //displays when no recipes are found
+    if(data.meals === null){
+        return result.innerHTML = '<p>Recipe cannot be found</p>'
+    }
+    else{
+        //displays recipe
+        const recipes = data.meals;
+        //this will display the names,pictures,area from,ingredients and instructions
+        const display = recipes.map(recipe => `
+           <div>
+            <h2>${recipe.strMeal}</h2>
+            <img src="${recipe.strMealThumb}" alt="${recipe.strMeal}">
+            <p>Area: ${recipe.strArea}</p>
+            <h3>Ingredients:</h3>
+            <ul>
+                <li>${handleIngredients(recipe)}</li>
+            <ul>
+            <h3>Instructions:</h3>
+            <p1>${recipe.strInstructions}</p1>
+          </div>
+        `).join('');
+            result.innerHTML = display;
+    }
+   })
+   .catch(error => {
+    console.error('An error occurred:', error.message);
+    result.innerHTML = '<p>An error occurred while fetching recipes.</p>';
+  });
+}
+
+// function to handle measure and ingredient display
+function handleIngredients(recipe){
+  let handleIngredientMeasure = '';// this empty string will be used to store list of ingredients and measures for a recipe in html lab
+
+  //It loops from 1 to 20 using variable i
+  for(let i=1; i <= 20; i++){
+     // Iterates through the recipe ingredients and measures 
+    const ingredient = recipe[`strIngredient${i}`];
+    const measure = recipe[`strMeasure${i}`];
+
+      // Check if ingredient exists and is not empty
+    if(ingredient.trim() !== ''){
+    // This makes the HTML list item for the ingredient and measure
+      handleIngredientMeasure += `<li>${measure} ${ingredient}<li>`;
+    }
+  }
+  return handleIngredientMeasure;// Returns string containing the HTML list items  
+}
